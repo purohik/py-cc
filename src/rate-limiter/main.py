9795bae27c5a3283a460fe43bd2c58c998c14458
puro <purohitkrishnar@gmail.com>
2024-07-26 21:23:31 +0530

    Changes in rate-limiter

--- src/rate-limiter/main.py
+++ src/rate-limiter/main.py
@@ -1,10 +1,26 @@
-from flask import Flask, request
+from flask import Flask
+from flask import request
+from flask import jsonify
+import asyncio
+import time
 
 app = Flask("Minimal Rate Limiter")
 
-@app.route('/test', methods=['GET'])
-def test_api():
-	return 'Test API WAI :)\n'
+@app.route("/streamer", methods=["GET"])
+def streaming_api():
+	def generate():
+		for x in range(5):
+			time.sleep(0.5)
+			yield "\nTime elapsed: " + str(x)
+	return app.response_class(generate())
+
+@app.route('/limited', methods=['GET'])
+def rate_limited():
+	return jsonify({"ip": request.remote_addr})
+
+@app.route("/unlimited", methods=["GET"])
+def unlimited():
+	return "No rate-limiting on this API.\n"
 
 if __name__ == '__main__':
 	app.run(debug=True)
